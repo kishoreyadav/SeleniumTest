11b02d167f6e2b2a27fd65aed49b9584f1dccbf6
518851 <518851@PC316530.cts.com>
2016-06-10 10:19:54 +0530

    My second commit

--- Tutorial1/src/App.java
+++ Tutorial1/src/App.java
@@ -7,6 +7,8 @@ public class App {
 	public static void main(String[] args) throws FileNotFoundException {
 		File f = new File("text.txt");
 		FileReader fr = new FileReader(f);
+		//this is my second commit 
+		//this will be show only after  my commit
 
 	}
 
